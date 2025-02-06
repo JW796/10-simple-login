5febdc861be4b54f275404c756ddd2f115de8273
James Joseph <98562132+JW796@users.noreply.github.com>
2025-02-06 09:45:07 -0500

    Add input fields

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from "react";
+
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
-      <h1 className="container bg-red-500 mx-auto">
+      <h1 className="text-center text-3xl font-semibold mt-2 py-4">
         10 - Simple Login useReducer
       </h1>
+      <form className="flex flex-col item">
+        <input
+          type="text"
+          autoComplete="username"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          autoComplete="username"
+          placeholder="Username"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </form>
     </>
   );
 }
