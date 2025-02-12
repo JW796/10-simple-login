898273c13e2c2d98380097ddc6b8e4148b5b55eb
James Joseph <98562132+JW796@users.noreply.github.com>
2025-02-12 11:36:19 -0500

    WIP 3 styling update + login error handling

--- src/App.js
+++ src/App.js
@@ -27,28 +27,45 @@ function App() {
       <h1 className="text-center text-3xl font-semibold mt-2 py-4">
         10 - Simple Login useReducer
       </h1>
-      <form className="flex flex-col items-center justify-center gap-4 mt-4 py-2" onSubmit={handleSumit}>
-        <input
-          className="border rounded-lg px-2 py-1"
-          type="text"
-          autoComplete="username"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          className="border rounded-lg px-2 py-1"
-          type="password"
-          autoComplete="username"
-          placeholder="Username"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="bg-blue-600 text-white text-lg font-medium rounded-lg py-1 px-3" type="submit">
-          Log in
-        </button>
-        <p className="text-red-500 text-center">{error}</p>
-      </form>
+      <div>
+        {loggedIn ? (
+          <>
+          <div>
+            <h2>Welcome {username}=!</h2>
+            <button>Log Out</button>
+          </div>
+          </>
+        ) : (
+          <form
+            className="flex flex-col items-center justify-center gap-4 mt-4 py-2"
+            onSubmit={handleSumit}
+          >
+            <input
+              className="border rounded-lg px-2 py-1"
+              type="text"
+              autoComplete="username"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              className="border rounded-lg px-2 py-1"
+              type="password"
+              autoComplete="username"
+              placeholder="Username"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              className="bg-blue-600 text-white text-lg font-medium rounded-lg py-1 px-3"
+              type="submit"
+            >
+              Log in
+            </button>
+            <p className="text-red-500 text-center">{error}</p>
+          </form>
+        )}
+      </div>
     </>
   );
 }
